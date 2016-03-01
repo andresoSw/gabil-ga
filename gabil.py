a70ad683ac1e057550f11d119bdf9698eb0045b6
andresosw jiemenz <andres_osw@hotmail.com>
2016-03-01 14:48:08 -0430

    Added Attribute list to binary stream helper class

---
+++ gabil.py
@@ -0,0 +1,28 @@
+class AttributeBinaryStream:
+
+	def __init__(self,attributes):
+		self.attributes = attributes
+		self.binaryStream = {}
+
+	def getStream(self):
+		return self.binaryStream
+
+	def setStream(self,attributes):
+		self.attributes = attributes
+
+	def computeBinaryStream(self):
+		number_of_attributes = len(self.attributes)
+		for index,attribute in enumerate(reversed(self.attributes)):
+			default_stream = list('0'*number_of_attributes)
+			#turning on the bit corresponding to the attribute
+			default_stream[index] = '1' 
+			self.binaryStream[attribute] = "".join(default_stream)
+
+	def printBinaryStream(self):
+		print 'BinaryStream: for attributes: %s\nStream: %s' %(self.attributes,self.binaryStream) 
+
+
+test_attributes = ['a','b','c','d']
+binaryStream = AttributeBinaryStream(test_attributes)
+binaryStream.computeBinaryStream()
+binaryStream.printBinaryStream()
